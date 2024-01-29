9fa8685e4629bace522033e518c5e7c345388894
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-29 19:23:36 +0530

    rating stars component

---
+++ client/src/components/RatingStars.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const RatingStars = ({ rating }) => {
+  let emptyStars = new Array(Math.round(5 - rating)).fill(0);
+  let fullStars = [];
+  let halfStars = [];
+  while (rating >= 0.5) {
+    if (rating >= 1) {
+      fullStars.push(0);
+      rating--;
+    } else if (rating >= 0.5) {
+      halfStars.push(0);
+      rating -= 0.5;
+    }
+  }
+  return (
+    <div className="flex items-center gap-1">
+      {fullStars.map((_) => (
+        <i class="bx bxs-star text-yellow-500"></i>
+      ))}
+      {halfStars.map((_) => (
+        <i class="bx bxs-star-half text-yellow-500"></i>
+      ))}
+      {emptyStars.map((_) => (
+        <i class="bx bxs-star text-slate-400"></i>
+      ))}
+    </div>
+  );
+};
+
+export default RatingStars;
