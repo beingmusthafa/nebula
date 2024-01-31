2fc3a6f7fda270c0ea330646a4c49dd45df51773
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-01 00:42:16 +0530

    error handler middleware

---
+++ api/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../utils/error.js";
+
+const errorHandler = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  console.log(error);
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  res.status(statusCode).json({ success: false, message });
+};
+
+export default errorHandler;
