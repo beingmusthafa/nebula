340fd8b6cda43751c63a05b803e7e6fbb3e13b5e
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-01 00:53:33 +0530

    session user interface

---
+++ api/interfaces/currentUser.interface.ts
@@ -0,0 +1,9 @@
+export default interface UsersInterface {
+  name: string;
+  email: string;
+  image: string;
+  role: ["user", "admin", "moderator"];
+  isBlocked: boolean;
+  appointmentCost: number;
+  interests: string[];
+}
