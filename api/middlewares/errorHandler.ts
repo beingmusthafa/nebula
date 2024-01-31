5491c7b6ab549cc7e62d122f8a0e0a7050078363
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-31 17:14:52 +0530

    error handler

--- api/middlewares/errorHandler.ts
+++ api/middlewares/errorHandler.ts
@@ -10,7 +10,7 @@ const errorHandler = (
   console.log(error);
   const statusCode = error.statusCode || 500;
   const message = error.message || "Internal Server Error";
-  res.status(statusCode).send({ success: false, message });
+  res.status(statusCode).json({ success: false, message });
 };
 
 export default errorHandler;
