bb25711b2029b3cb8becbabae1daa9cde44662aa
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-01 23:02:04 +0530

    req.user 's interface

--- api/interfaces/currentUser.interface.ts
+++ api/interfaces/currentUser.interface.ts
@@ -1,8 +1,11 @@
-export default interface UsersInterface {
+import mongoose from "mongoose";
+
+export default interface CurrentUserInterface {
+  _id: mongoose.Types.ObjectId;
   name: string;
   email: string;
   image: string;
-  role: ["user", "admin", "moderator"];
+  role: "user" | "admin" | "moderator";
   isBlocked: boolean;
   appointmentCost: number;
   interests: string[];
