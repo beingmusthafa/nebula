34c2c2f314960a9cace4ce854cf75494d6aa9e09
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-04 22:08:22 +0530

    admin route protection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer, Slide } from "react-toastify";
 import Header from "./components/Header";
-import Sidebar from "./components/admin/Sidebar.tsx";
 import Loading from "./components/Loading";
-const Home = lazy(() => import("./pages/Home"));
+import AdminAuth from "./components/auth/AdminAuth.tsx";
+import NotFound from "./pages/error/NotFound.tsx";
+import UserAuth from "./components/auth/UserAuth.tsx";
+import TutorDashboard from "./pages/tutor/TutorDashboard.tsx";
+import AddCourse from "./pages/tutor/AddCourse.tsx";
+const Home = lazy(() => import("./pages/user/Home.tsx"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const SignIn = lazy(() => import("./pages/SignIn"));
 const Users = lazy(() => import("./pages/admin/Users"));
@@ -14,6 +19,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <ToastContainer transition={Slide} />
       <Routes>
         <Route
           path="/"
@@ -31,6 +37,9 @@ const App = () => {
             </Suspense>
           }
         />
+        <Route element={<UserAuth />}>
+          <Route path="/sign-out" element={<h1>Sign Out</h1>} />
+        </Route>
         <Route
           path="/sign-in"
           element={
@@ -47,30 +56,36 @@ const App = () => {
             </Suspense>
           }
         />
-        <Route
-          path="/admin/users"
-          element={
-            <Suspense fallback={<Loading />}>
-              <Users />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/admin/courses"
-          element={
-            <Suspense fallback={<Loading />}>
-              <Users />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/admin/users/user-details/:id"
-          element={
-            <Suspense fallback={<Loading />}>
-              <UserDetails />
-            </Suspense>
-          }
-        />
+        <Route path="/tutor" element={<TutorDashboard />} />
+        <Route path="/tutor/add-course" element={<AddCourse />} />
+        {/* Admin routes */}
+        <Route path="/admin/*" element={<AdminAuth />}>
+          <Route
+            path="users"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Users />
+              </Suspense>
+            }
+          />
+          <Route
+            path="courses"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Users />
+              </Suspense>
+            }
+          />
+          <Route
+            path="users/user-details/:id"
+            element={
+              <Suspense fallback={<Loading />}>
+                <UserDetails />
+              </Suspense>
+            }
+          />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
