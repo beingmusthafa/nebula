0eb5d343d0c70f859abc7b2bf1981225333d80fe
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-08 00:55:54 +0530

    added isActive and language in courses schema

--- api/models/courses.model.ts
+++ api/models/courses.model.ts
@@ -1,53 +1,73 @@
 import mongoose from "mongoose";
 
-const coursesModel = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const coursesModel = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "Categories",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    ratingCount: {
+      type: Number,
+      default: 0,
+    },
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    requirements: {
+      type: [String],
+      default: [],
+    },
+    benefits: {
+      type: [String],
+      default: [],
+    },
+    offers: {
+      type: [
+        {
+          type: mongoose.Types.ObjectId,
+          default: [],
+        },
+      ],
+      ref: "Offers",
+      default: [],
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: mongoose.Types.ObjectId,
-    ref: "Categories",
-    required: true,
-  },
-  author: {
-    type: mongoose.Types.ObjectId,
-    ref: "Users",
-    required: true,
-  },
-  requirements: {
-    type: [String],
-    default: [],
-  },
-  benefits: {
-    type: [String],
-    default: [],
-  },
-  offers: {
-    type: [
-      {
-        type: mongoose.Types.ObjectId,
-        default: [],
-      },
-    ],
-    default: [],
-  },
-  isBlocked: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Courses", coursesModel);
