e2cd0ccab35051992743027787631bb07c66ccbc
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-08 00:02:40 +0530

    header full responsive design completed

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,74 +1,173 @@
-import React from "react";
+import React, { useState } from "react";
 import lightLogo from "../assets/nebula_light.png";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { signIn, signOut } from "../redux/user/userSlice";
 
 const Header = () => {
-  // const { currentUser } = useSelector((state) => state.user);
-  // const dispatch = useDispatch();
-  // console.log(currentUser);
+  const { currentUser } = useSelector((state: any) => state.user);
+  let [showOptions, setShowOptions] = useState(false);
+  let [showLogoutConfirm, setShowLogoutConfirm] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  console.log(currentUser);
   const location = useLocation();
-  const currentUser = false;
-
+  const logout = () => {
+    dispatch(signOut());
+    setShowOptions(false);
+    setShowLogoutConfirm(false);
+    navigate("/sign-in");
+  };
+  const navigateOptions = (route: string) => {
+    setShowOptions(false);
+    navigate(route);
+  };
   return (
     !location.pathname.startsWith("/admin") && (
-      <div className="flex justify-between items-center sticky top-0 left-0 w-full px-4 border-b-4 _border-blue-black-gradient2 bg-white z-10">
+      <div className="flex justify-between items-center h-14 sticky top-0 left-0 w-full px-4 border-b-4 _border-blue-black-gradient2 bg-white z-10">
         <Link to={"/"} className="flex items-center gap-2 cursor-pointer">
           <img
             src={lightLogo}
             alt=""
-            height={60}
-            width={60}
-            className="sm:w-8 sm:h-8 md:w-14 md:h-14"
+            height={50}
+            width={50}
+            className="sm:w-8 sm:h-8 md:w-12 md:h-12"
           />
           <h1 className="_font-tilt-warp text-3xl _color-blue _text-blue-black-gradient cursor-pointer hidden md:block">
             nebula
           </h1>
         </Link>
-        <form action="" className="flex justify-center">
+        <form
+          action=""
+          className="flex justify-center border border-black py-1 px-4 rounded-full"
+        >
           <input
             type="text"
-            placeholder="Search for courses or tutors"
-            className=" border border-black pl-4 w-80"
+            placeholder="Search for courses"
+            className=" pl-4 w-44 md:w-80 border-0"
           />
-          <button className="_fill-btn-blue">
+          <button className="ml-2">
             <i className="bx bx-search-alt-2 text-lg"></i>
           </button>
         </form>
-        <div className="flex md:gap-8 gap-4 items-center cursor-pointer">
+        <div className="md:gap-8 gap-4 items-center cursor-pointer hidden md:flex">
           {currentUser ? (
             <>
-              <Link to={"/admin"}>
-                <i className="bx bxs-key text-3xl cursor-pointer"></i>
-              </Link>
+              <button
+                className="text-red-500 font-semibold"
+                onClick={() => setShowLogoutConfirm(true)}
+              >
+                Logout
+              </button>
               <Link to={"/wishlist"}>
-                <i className="bx bx-bookmarks text-3xl cursor-pointer"></i>
+                <i className="bx bx-heart text-2xl cursor-pointer"></i>
               </Link>
               <Link to={"/cart"}>
-                <i className="bx bx-cart-alt text-3xl cursor-pointer"></i>
+                <i className="bx bx-cart-alt text-2xl cursor-pointer"></i>
               </Link>
               <Link to={"/tutor"}>
-                <i className="bx bxs-graduation text-3xl cursor-pointer"></i>
+                <i className="bx bxs-graduation text-2xl cursor-pointer"></i>
               </Link>
               <img
-                src="https://htmlcolorcodes.com/assets/images/colors/bright-yellow-color-solid-background-1920x1080.png"
-                alt=""
-                className="max-h-fit h-10 w-10 rounded-full cursor-pointer"
+                src={currentUser.image}
+                className="max-h-fit h-8 w-8 rounded-full cursor-pointer"
               />
             </>
           ) : (
             <>
-              <Link to={"/sign-in"} className="_fill-btn-blue">
+              <Link to={"/sign-in"} className="_fill-btn-blue2">
                 Sign in
               </Link>
-              <Link to={"/sign-up"} className="_fill-btn-blue">
+              <Link to={"/sign-up"} className="_fill-btn-blue2">
                 Sign up
               </Link>
             </>
           )}
         </div>
+        <div className="flex md:hidden">
+          <i
+            onClick={() => setShowOptions(!showOptions)}
+            className="bx bx-menu text-3xl text-black"
+          ></i>
+        </div>
+        {currentUser && showOptions && (
+          <div className="absolute flex md:hidden flex-col top-14 right-0 bg-white px-4">
+            <div
+              onClick={() => navigateOptions("/wishlist")}
+              className="flex items-center gap-2 border-b border-slate-400 p-2"
+            >
+              <i className="bx bx-heart text-2xl"></i>
+              Wishlist
+            </div>
+            <div
+              onClick={() => navigateOptions("/cart")}
+              className="flex items-center gap-2 border-b border-slate-400 p-2"
+            >
+              <i className="bx bx-cart-alt text-2xl"></i>
+              Cart
+            </div>
+            <div
+              onClick={() => navigateOptions("/tutor")}
+              className="flex items-center gap-2 border-b border-slate-400 p-2"
+            >
+              <i className="bx bxs-graduation text-2xl "></i>
+              Tutor
+            </div>
+            <div
+              onClick={() => navigateOptions("/profile")}
+              className="flex items-center gap-2 p-2"
+            >
+              <img
+                src={currentUser.image}
+                className="max-h-fit h-8 w-8 rounded-full"
+              />
+              Profile
+            </div>
+            <button
+              className="text-red-500 font-semibold"
+              onClick={() => setShowLogoutConfirm(true)}
+            >
+              Logout
+            </button>
+          </div>
+        )}
+        {!currentUser && showOptions && (
+          <div className="absolute flex md:hidden flex-col top-14 right-0 bg-white p-2 gap-2 text-sky-500">
+            <button
+              onClick={() => navigateOptions("/sign-in")}
+              className="uppercase font-bold p-2 border-b border-slate-300"
+            >
+              sign in
+            </button>
+            <button
+              onClick={() => navigateOptions("/sign-up")}
+              className="uppercase font-bold p-2"
+            >
+              sign up
+            </button>
+          </div>
+        )}
+        {showLogoutConfirm && (
+          <div className="flex fixed w-full justify-center">
+            <div className="flex flex-col p-6 fixed top-1/3 border-2 border-black bg-white">
+              <div className="text-base font-medium">
+                Are you sure you want to logout?
+              </div>
+              <div className="flex gap-10 mx-auto mt-4">
+                <button
+                  onClick={() => setShowLogoutConfirm(false)}
+                  className="_fill-btn-black"
+                >
+                  Cancel
+                </button>
+                <button onClick={logout} className="_fill-btn-red">
+                  Yes
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     )
   );
