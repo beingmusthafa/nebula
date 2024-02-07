d1e4671c6f0342a9acda514d4ec913e36eea45b3
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-07 13:15:33 +0530

    tutor routes

--- api/index.ts
+++ api/index.ts
@@ -4,10 +4,13 @@ import errorHandler from "./middlewares/errorHandler.middleware.js";
 import authRouter from "./routes/auth.router.js";
 import adminRouter from "./routes/admin.router.js";
 import userRouter from "./routes/user.router.js";
+import tutorRouter from "./routes/tutor.router.js";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import coursesModel from "./models/courses.model.js";
 const app = express();
 connectDb();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -20,6 +23,7 @@ app.use(
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/admin", adminRouter);
+app.use("/api/tutor", tutorRouter);
 app.use("/api", userRouter);
 
 app.use(errorHandler);
@@ -27,5 +31,5 @@ app.use("*", (req: Request, res: Response) => {
   res.status(404).json({ message: "Page not found" });
 });
 app.listen(3000, () => {
-  console.log("server started");
+  console.log("Server started");
 });
