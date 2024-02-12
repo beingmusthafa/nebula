71b9fb7aa34061ef18b25aba6a9247ff2a71ffcb
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-13 01:10:39 +0530

    ...

--- client/src/components/CourseCard.tsx
+++ client/src/components/CourseCard.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import RatingStars from "./RatingStars";
+import { Link } from "react-router-dom";
 
 interface Props {
   course: {
+    _id: string;
     title: string;
     rating: number;
-    price: string;
+    ratingCount: number;
+    price: number;
     thumbnail: string;
     tutor: {
       name: string;
@@ -16,8 +19,11 @@ interface Props {
 
 const CourseCard: React.FC<Props> = ({ course }) => {
   return (
-    <div className="flex flex-col items-start w-fit">
-      <img src={course.thumbnail} className="object-cover w-72 h-36" alt="" />
+    <Link
+      to={`/course-details/${course._id}`}
+      className="flex flex-col items-start w-fit"
+    >
+      <img src={course.thumbnail} className="object-cover w-64 h-36" alt="" />
       <div className="font-bold text-lg text-wrap" style={{ width: 270 }}>
         {course.title}
       </div>
@@ -27,10 +33,10 @@ const CourseCard: React.FC<Props> = ({ course }) => {
       </div>
       <div className="flex items-center">
         <span className="_font-tilt-warp mr-2 text-lg">{course.rating}</span>
-        <RatingStars rating={course.rating} />
+        <RatingStars rating={course.rating} />({course.ratingCount})
       </div>
-      <p className="font-bold text-lg">{course.price}</p>
-    </div>
+      <p className="font-bold text-lg">&#8377; {course.price}</p>
+    </Link>
   );
 };
 
---
+++ client/src/components/skeletons/CourseSkeleton.tsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion";
+
+const CourseSkeleton = () => {
+  return (
+    <div className="flex flex-col items-start w-fit gap-2">
+      <motion.div
+        initial={{ opacity: 0.3 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7, repeatType: "mirror", repeat: Infinity }}
+        className=" h-36 bg-slate-300"
+        style={{ width: 270 }}
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 0.3 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7, repeatType: "mirror", repeat: Infinity }}
+        className="w-72 h-4 bg-slate-300"
+        style={{ width: 270 }}
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 0.3 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7, repeatType: "mirror", repeat: Infinity }}
+        className="w-48 h-4 bg-slate-300"
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 0.3 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7, repeatType: "mirror", repeat: Infinity }}
+        className="w-48 h-4 bg-slate-300"
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 0.3 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7, repeatType: "mirror", repeat: Infinity }}
+        className="w-20 h-4 bg-slate-300"
+      ></motion.div>
+    </div>
+  );
+};
+
+export default CourseSkeleton;
