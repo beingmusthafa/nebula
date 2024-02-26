3ac4f349542f4e31c5295dacf5157fff08ae5f3e
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-02-27 04:46:17 +0530

    stats and dashboard

---
+++ client/src/pages/admin/Stats_admin.tsx
@@ -0,0 +1,157 @@
+import React, { useEffect, useState } from "react";
+import LineChartComponent from "../../components/tutor/LineChart";
+import AdminLoading from "../../components/admin/AdminLoading";
+import CourseCard from "../../components/CourseCard";
+import CourseSkeleton from "../../components/skeletons/CourseSkeleton";
+import { useNavigate } from "react-router-dom";
+let monthNames = [
+  "",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+interface Course {
+  _id: string;
+  title: string;
+  rating: number;
+  ratingCount: number;
+  price: number;
+  discount: number;
+  thumbnail: string;
+  tutor: {
+    name: string;
+    image: string;
+  };
+}
+const Stats = () => {
+  const currentMonth = new Date().getMonth() + 1;
+  let [currentEnrollments, setCurrentEnrollments] = useState(0);
+  let [currentRevenue, setCurrentRevenue] = useState(0);
+  let [loading, setLoading] = useState(true);
+  let [enrollments, setEnrollments] = useState<number[]>([]);
+  let [revenue, setRevenue] = useState<number[]>([]);
+  let [months, setMonths] = useState<string[]>([]);
+  let [topLoading, setTopLoading] = useState(true);
+  let [topCourses, setTopCourses] = useState<{ data: Course; count: number }[]>(
+    []
+  );
+  const skeletons = new Array(5).fill(0);
+  const navigate = useNavigate();
+  const getGraphData = async () => {
+    try {
+      const res = await fetch("/api/admin/get-graph-data").then((res) =>
+        res.json()
+      );
+      setLoading(false);
+      if (!res.success) throw new Error(res.message);
+      console.log(res.courseEnrollmentData);
+      let enrollmentArray: number[] = [];
+      let revenueArray: number[] = [];
+      let monthArray: string[] = [];
+      res.courseEnrollmentData.forEach((data: any) => {
+        enrollmentArray.push(data.count);
+        revenueArray.push(data.revenue);
+        monthArray.push(monthNames[data.month]);
+        if (data.month === currentMonth) {
+          setCurrentEnrollments(data.count);
+          setCurrentRevenue(data.revenue);
+        }
+      });
+      setEnrollments(enrollmentArray);
+      setRevenue(revenueArray);
+      setMonths(monthArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchTopCourses = async () => {
+    try {
+      setTopLoading(true);
+      const res = await fetch("/api/admin/get-top-courses").then((res) =>
+        res.json()
+      );
+      setTopLoading(false);
+      console.log({ top: res.courses });
+      if (!res.success) throw new Error(res.message);
+      setTopCourses(res.courses);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getGraphData();
+    fetchTopCourses();
+  }, []);
+  console.log({ enrollments, revenue, months });
+  return loading ? (
+    <AdminLoading />
+  ) : (
+    <>
+      <div className="flex flex-col md:flex-row justify-center h-80 my-10">
+        <div className="h-80 w-full md:w-[50vw]">
+          <LineChartComponent
+            data1={enrollments}
+            data2={revenue}
+            xLabels={months}
+          />
+        </div>
+        <div className="flex flex-row md:flex-col justify-evenly">
+          <div className="h-fit flex flex-col items-center p-2 border-4 w-32 rounded-2xl border-cyan-300">
+            <p className="text-slate-500">Enrollments</p>
+            <p className="font-bold text-2xl text-slate-700">
+              {currentEnrollments}
+            </p>
+          </div>
+          <div className="h-fit flex flex-col items-center p-2 border-4 w-32 rounded-2xl border-cyan-300">
+            <p className="text-slate-500">Revenue</p>
+            <p className="font-bold text-2xl text-slate-700">
+              &#8377; {currentRevenue}
+            </p>
+          </div>
+          <div className="h-fit flex flex-col items-center p-2 border-4 w-32 rounded-2xl border-cyan-300">
+            <p className="text-slate-500">Earnings</p>
+            <p className="font-bold text-2xl text-slate-700">
+              &#8377; {Math.round((currentRevenue / 100) * 20)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="_section-title">Bestselling courses</div>
+      {topLoading ? (
+        <div className="flex gap-4 px-6 justify-start flex-wrap bg-white">
+          {skeletons.map((_, index) => (
+            <CourseSkeleton key={index} />
+          ))}
+        </div>
+      ) : topCourses?.length > 0 ? (
+        <>
+          <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+            {topCourses?.map((course) => (
+              <CourseCard
+                redirectTo={"/tutor/manage-course-content/" + course.data?._id}
+                key={course.data?._id}
+                course={course.data}
+                showTutor={false}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="text-2xl text-slate-400 _font-dm-display w-full text-center my-24">
+          No top courses
+        </p>
+      )}
+    </>
+  );
+};
+
+export default Stats;
--- client/src/pages/tutor/TutorDashboard.tsx
+++ client/src/pages/tutor/TutorDashboard.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import CourseCard from "../../components/user/CourseCard";
+import CourseCard from "../../components/CourseCard";
 import CourseSkeleton from "../../components/skeletons/CourseSkeleton";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import ConfirmationPopup from "../../components/ConfirmationPopup";
+import LineChartComponent from "../../components/tutor/LineChart";
 
 interface Course {
   _id: string;
@@ -18,27 +19,178 @@ interface Course {
     image: string;
   };
 }
+let monthNames = [
+  "",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const TutorDashboard = () => {
-  let skeletons = new Array(7).fill(0);
+  let skeletons = new Array(4).fill(0);
   const navigate = useNavigate();
   let [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+  let [showMakePublishConfirm, setShowMakePublishConfirm] = useState(false);
+  let [showCancelPublishConfirm, setShowCancelPublishConfirm] = useState(false);
   let [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
-  let [courses, setCourses] = useState<Course[]>([]);
-  const fetchCourses = async () => {
+  let [creatingCourses, setCreatingCourses] = useState<Course[]>([]);
+  let [pendingCourses, setPendingCourses] = useState<Course[]>([]);
+  let [publishedCourses, setPublishedCourses] = useState<Course[]>([]);
+  let [creatingLoading, setCreatingLoading] = useState(false);
+  let [pendingLoading, setPendingLoading] = useState(false);
+  let [publishedLoading, setPublishedLoading] = useState(false);
+  //stats related states
+  const currentMonth = new Date().getMonth() + 1;
+  let [currentEnrollments, setCurrentEnrollments] = useState(0);
+  let [currentRevenue, setCurrentRevenue] = useState(0);
+  let [loading, setLoading] = useState(true);
+  let [enrollments, setEnrollments] = useState<number[]>([]);
+  let [revenue, setRevenue] = useState<number[]>([]);
+  let [months, setMonths] = useState<string[]>([]);
+  let [topLoading, setTopLoading] = useState(true);
+  let [topCourses, setTopCourses] = useState<{ data: Course; count: number }[]>(
+    []
+  );
+  const fetchTopCourses = async () => {
     try {
-      const res = await fetch("/api/tutor/get-all-courses").then((res) =>
+      setTopLoading(true);
+      const res = await fetch("/api/admin/get-top-courses").then((res) =>
+        res.json()
+      );
+      setTopLoading(false);
+      console.log({ top: res.courses });
+      if (!res.success) throw new Error(res.message);
+      setTopCourses(res.courses);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getGraphData = async () => {
+    try {
+      const res = await fetch("/api/tutor/get-graph-data").then((res) =>
+        res.json()
+      );
+      setLoading(false);
+      if (!res.success) throw new Error(res.message);
+      console.log(res.courseEnrollmentData);
+      let enrollmentArray: number[] = [];
+      let revenueArray: number[] = [];
+      let monthArray: string[] = [];
+      res.courseEnrollmentData.forEach((data: any) => {
+        enrollmentArray.push(data.count);
+        revenueArray.push(data.revenue);
+        monthArray.push(monthNames[data.month]);
+        if (data.month === currentMonth) {
+          setCurrentEnrollments(data.count);
+          setCurrentRevenue(data.revenue);
+        }
+      });
+      setEnrollments(enrollmentArray);
+      setRevenue(revenueArray);
+      setMonths(monthArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchCreatingCourses = async () => {
+    try {
+      setCreatingLoading(true);
+      const res = await fetch("/api/tutor/get-creating-courses").then((res) =>
         res.json()
       );
       if (!res.success) return toast.error(res.message);
-      setCourses(res.docs);
-      console.log(res.docs);
+      setCreatingCourses(res.courses);
+      setCreatingLoading(false);
     } catch (error) {
+      setCreatingLoading(false);
+      toast.error("Something went wrong");
+    }
+  };
+  const fetchPendingCourses = async () => {
+    try {
+      setPendingLoading(true);
+      const res = await fetch("/api/tutor/get-pending-courses").then((res) =>
+        res.json()
+      );
+      if (!res.success) return toast.error(res.message);
+      setPendingCourses(res.courses);
+      setPendingLoading(false);
+    } catch (error) {
+      setPendingLoading(false);
+      toast.error("Something went wrong");
+    }
+  };
+  const fetchPublishedCourses = async () => {
+    try {
+      setPublishedLoading(true);
+      const res = await fetch("/api/tutor/get-published-courses").then((res) =>
+        res.json()
+      );
+      if (!res.success) return toast.error(res.message);
+      setPublishedCourses(res.courses);
+      setPublishedLoading(false);
+    } catch (error) {
+      setPublishedLoading(false);
       toast.error("Something went wrong");
     }
   };
   useEffect(() => {
-    fetchCourses();
+    fetchCreatingCourses();
+    fetchPendingCourses();
+    fetchPublishedCourses();
+    getGraphData();
+    fetchTopCourses();
   }, []);
+  const makePublishRequest = async () => {
+    setShowMakePublishConfirm(false);
+    const toastId = toast.loading("Making publish request");
+    try {
+      const res = await fetch(
+        "/api/tutor/make-publish-request/" + selectedCourse?._id,
+        {
+          method: "PATCH",
+        }
+      ).then((res) => res.json());
+      toast.dismiss(toastId);
+      if (!res.success) throw new Error(res.message);
+      fetchCreatingCourses();
+      fetchPendingCourses();
+      setPublishedLoading(false);
+      setCreatingLoading(false);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+  const cancelPublishRequest = async () => {
+    setShowCancelPublishConfirm(false);
+    const toastId = toast.loading("Cancelling publish request");
+    try {
+      const res = await fetch(
+        "/api/tutor/cancel-publish-request/" + selectedCourse?._id,
+        {
+          method: "PATCH",
+        }
+      ).then((res) => res.json());
+      toast.dismiss(toastId);
+      console.log({ res });
+      if (!res.success) throw new Error(res.message);
+      fetchCreatingCourses();
+      fetchPendingCourses();
+      setPublishedLoading(false);
+      setCreatingLoading(false);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
   const handleDelete = async () => {
     const toastId = toast.loading("Deleting course");
     try {
@@ -51,7 +203,7 @@ const TutorDashboard = () => {
       toast.dismiss(toastId);
       if (!res.success) return toast.error(res.message);
       toast.success(res.message);
-      fetchCourses();
+      fetchCreatingCourses();
       setShowDeleteConfirm(false);
       setSelectedCourse(null);
     } catch (error) {
@@ -69,26 +221,103 @@ const TutorDashboard = () => {
           onConfirm={handleDelete}
         />
       )}
+      {showMakePublishConfirm && (
+        <ConfirmationPopup
+          isActionPositive={false}
+          onCancel={() => setShowMakePublishConfirm(false)}
+          confirmText={`Make publish request for this course?: ${selectedCourse?.title}`}
+          onConfirm={makePublishRequest}
+        />
+      )}
+      {showCancelPublishConfirm && (
+        <ConfirmationPopup
+          isActionPositive={false}
+          onCancel={() => setShowCancelPublishConfirm(false)}
+          confirmText={`Cancel publish request for this course?: ${selectedCourse?.title}`}
+          onConfirm={cancelPublishRequest}
+        />
+      )}
       <Link to="/tutor/add-course" className="_fill-btn-blue">
         Add course
       </Link>
-      <div className="_section-title">Your courses</div>
-      <div className="flex gap-4 px-6 justify-start flex-wrap bg-white">
-        {courses.length > 0
-          ? courses?.map((course) => (
+      <div className="flex flex-col md:flex-row justify-center h-80 my-10">
+        <div className="h-80 w-full md:w-[50vw]">
+          <LineChartComponent
+            data1={enrollments}
+            data2={revenue}
+            xLabels={months}
+          />
+        </div>
+        <div className="flex flex-row md:flex-col justify-evenly">
+          <div className="h-fit flex flex-col items-center p-2 border-4 w-32 rounded-2xl border-cyan-300">
+            <p className="text-slate-500">Enrollments</p>
+            <p className="font-bold text-2xl text-slate-700">
+              {currentEnrollments}
+            </p>
+          </div>
+          <div className="h-fit flex flex-col items-center p-2 border-4 w-32 rounded-2xl border-cyan-300">
+            <p className="text-slate-500">Revenue</p>
+            <p className="font-bold text-2xl text-slate-700">
+              &#8377; {currentRevenue}
+            </p>
+          </div>
+          <div className="h-fit flex flex-col items-center p-2 border-4 w-32 rounded-2xl border-cyan-300">
+            <p className="text-slate-500">Earnings</p>
+            <p className="font-bold text-2xl text-slate-700">
+              &#8377; {Math.round((currentRevenue / 100) * 20)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="_section-title">Bestselling courses</div>
+      {topLoading ? (
+        <div className="flex gap-4 px-6 justify-start flex-wrap bg-white">
+          {skeletons.map((_, index) => (
+            <CourseSkeleton key={index} />
+          ))}
+        </div>
+      ) : topCourses?.length > 0 ? (
+        <>
+          <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+            {topCourses?.map((course) => (
+              <CourseCard
+                redirectTo={"/tutor/manage-course-content/" + course.data?._id}
+                key={course.data?._id}
+                course={course.data}
+                showTutor={false}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="text-2xl text-slate-400 _font-dm-display w-full text-center my-24">
+          No top courses
+        </p>
+      )}
+      <div className="_section-title">Courses under creation</div>
+      {creatingLoading ? (
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+          {skeletons.map((_, index) => (
+            <CourseSkeleton key={index} />
+          ))}
+        </div>
+      ) : creatingCourses.length > 0 ? (
+        <>
+          <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+            {creatingCourses?.map((course) => (
               <CourseCard
                 redirectTo={"/tutor/manage-course-content/" + course._id}
                 key={course._id}
                 course={course}
                 showTutor={false}
-                extraElement={
-                  <div className="flex gap-2">
+                extraElements={
+                  <div className="flex flex-col gap-2">
                     <button
                       onClick={() => {
                         setShowDeleteConfirm(true);
                         setSelectedCourse(course);
                       }}
-                      className="_fill-btn-blue"
+                      className="_fill-btn-red"
                     >
                       <i className="bx bx-trash-alt text-base"></i>
                     </button>
@@ -100,12 +329,101 @@ const TutorDashboard = () => {
                     >
                       <i className="bx bx-edit text-base"></i>
                     </button>
+                    <button
+                      onClick={() => {
+                        setSelectedCourse(course);
+                        setShowMakePublishConfirm(true);
+                      }}
+                      className="_fill-btn-blue"
+                    >
+                      <i className="bx bx-upvote text-base"></i>
+                    </button>
                   </div>
                 }
               />
-            ))
-          : skeletons.map((_, index) => <CourseSkeleton key={index} />)}
-      </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="text-2xl text-slate-400 _font-dm-display w-full text-center my-24">
+          No creating courses
+        </p>
+      )}
+      <div className="_section-title">Pending courses</div>
+      {pendingLoading ? (
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+          {skeletons.map((_, index) => (
+            <CourseSkeleton key={index} />
+          ))}
+        </div>
+      ) : pendingCourses.length > 0 ? (
+        <>
+          <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+            {pendingCourses?.map((course) => (
+              <CourseCard
+                redirectTo={"/tutor/manage-course-content/" + course._id}
+                key={course._id}
+                course={course}
+                showTutor={false}
+                extraElements={
+                  <div className="flex flex-col gap-2">
+                    <button
+                      onClick={() => {
+                        setSelectedCourse(course);
+                        setShowCancelPublishConfirm(true);
+                      }}
+                      className="_fill-btn-blue"
+                    >
+                      <i className="bx bx-x-circle text-base"></i>
+                    </button>
+                  </div>
+                }
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="text-2xl text-slate-400 _font-dm-display w-full text-center my-24">
+          No pending courses
+        </p>
+      )}
+      <div className="_section-title">Published courses</div>
+      {publishedLoading ? (
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+          {skeletons.map((_, index) => (
+            <CourseSkeleton key={index} />
+          ))}
+        </div>
+      ) : publishedCourses.length > 0 ? (
+        <>
+          <div className="flex flex-col md:flex-row items-center md:items-start gap-4 px-6 justify-start flex-wrap bg-white">
+            {publishedCourses?.map((course) => (
+              <CourseCard
+                redirectTo={"/tutor/manage-course-content/" + course._id}
+                key={course._id}
+                course={course}
+                showTutor={false}
+                extraElements={
+                  <div className="flex flex-col gap-2">
+                    <button
+                      onClick={() =>
+                        navigate("/tutor/edit-course/" + course._id)
+                      }
+                      className="_fill-btn-blue"
+                    >
+                      <i className="bx bx-edit text-base"></i>
+                    </button>
+                  </div>
+                }
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="text-2xl text-slate-400 _font-dm-display w-full text-center my-24">
+          No published courses
+        </p>
+      )}
     </>
   );
 };
