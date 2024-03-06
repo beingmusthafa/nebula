5c48a8469fd646a31f588e2d56859f77303b3a9d
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-06 21:58:12 +0530

    jwt null issue fix

--- api/middlewares/auth.middleware.ts
+++ api/middlewares/auth.middleware.ts
@@ -25,29 +25,10 @@ class AuthMiddleware {
   async userIdentify(req: Request, res: Response, next: NextFunction) {
     try {
       const token = req.headers.authorization.split(" ")[1];
-      if (!token) return next();
+      if (!token || token === "null") return next();
       const { id } = jwt.verify(token, process.env.JWT_SECRET);
       if (!id) return next();
-      //TEST MODE/////////////////////////////////////////////////////////////////////////////
-      // req.session.user = {
-      //   _id: "65c38a47a80c66ffa1dd30ef",
-      //   name: "Muhammad Musthafa",
-      //   email: "musthafarebel48@gmail.com",
-      //   image:
-      //     "https://res.cloudinary.com/dfezowkdc/image/upload/v1706951267/gray-photo-placeholder-icon-design-ui-vector-35850819_vupnvf.jpg",
-      //   role: "admin",
-      //   isBlocked: false,
-      //   appointmentCost: 0,
-      //   interests: [
-      //     "65c9891ee5db19cf23ee8791",
-      //     "65c98936e5db19cf23ee8797",
-      //     "65c989e8e5db19cf23ee87a0",
-      //     "65c989a0e5db19cf23ee879d",
-      //   ],
-      //   education: [],
-      //   experience: [],
-      // };
-      // TEST MODE/////////////////////////////////////////////////////////////////////////////
+
       if (!req.session.user) {
         req.session.user = await this.usersRepository.findById(id, {
           _id: 1,
@@ -73,69 +54,55 @@ class AuthMiddleware {
 
   async userAuth(req: Request, res: Response, next: NextFunction) {
     try {
-      const authHeader = req.headers.authorization;
-      if (authHeader && authHeader.startsWith("Bearer ")) {
-        const token = authHeader.split(" ")[1];
-        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      const token = req.headers.authorization.split(" ")[1];
+      if (!token || token === "null") throw customError(401, "Unauthorized");
+      const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      if (!id) throw customError(403, "Invalid token, login again");
 
-        if (!id) {
-          req.session.user = null;
-          return next(customError(403, "Invalid token, login again"));
-        }
-
-        if (!req.session.user) {
-          req.session.user = await this.usersRepository.findById(id, {
-            _id: 1,
-            name: 1,
-            email: 1,
-            image: 1,
-            role: 1,
-            isBlocked: 1,
-            appointmentCost: 1,
-            interests: 1,
-          });
-        }
-
-        if (req.session.user.isBlocked) {
-          req.session.user = null;
-          return next(customError(403, "Your account is suspended"));
-        }
+      if (!req.session.user) {
+        req.session.user = await this.usersRepository.findById(id, {
+          _id: 1,
+          name: 1,
+          email: 1,
+          image: 1,
+          role: 1,
+          isBlocked: 1,
+          appointmentCost: 1,
+          interests: 1,
+        });
+      }
+      if (req.session.user.isBlocked) {
+        req.session.user = null;
+        throw customError(403, "Your account is suspended");
       }
       next();
     } catch (error) {
-      console.error(error);
+      console.log(error);
       next(error);
     }
   }
 
   async adminAuth(req: Request, res: Response, next: NextFunction) {
     try {
-      const authHeader = req.headers.authorization;
-      if (authHeader && authHeader.startsWith("Bearer ")) {
-        const token = authHeader.split(" ")[1];
-        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      const token = req.headers.authorization.split(" ")[1];
+      if (!token || token === "null") throw customError(401, "Unauthorized");
+      const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      if (!id) throw customError(403, "Invalid token, login again");
 
-        if (!id) {
-          req.session.user = null;
-          return next(customError(403, "Invalid token, login again"));
-        }
-
-        if (!req.session.user) {
-          req.session.user = await this.usersRepository.findById(id, {
-            _id: 1,
-            name: 1,
-            email: 1,
-            image: 1,
-            role: 1,
-            isBlocked: 1,
-            appointmentCost: 1,
-            interests: 1,
-          });
-        }
-        if (req.session.user.role !== "admin" || req.session.user.isBlocked) {
-          req.session.user = null;
-          return next(customError(403, "Forbidden"));
-        }
+      if (!req.session.user) {
+        req.session.user = await this.usersRepository.findById(id, {
+          name: 1,
+          email: 1,
+          image: 1,
+          role: 1,
+          isBlocked: 1,
+          appointmentCost: 1,
+          interests: 1,
+        });
+      }
+      if (req.session.user.role !== "admin" || req.session.user.isBlocked) {
+        req.session.user = null;
+        throw customError(403, "Forbidden");
       }
       next();
     } catch (error) {
