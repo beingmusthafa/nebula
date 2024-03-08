f0c5cb1da6dfca8888ded7520cb7ac1a6c1f67b3
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-08 20:32:53 +0530

    refresh cart after payment success

--- client/src/pages/user/PaymentSuccess.tsx
+++ client/src/pages/user/PaymentSuccess.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
+import { CartWishlistContext } from "../../components/context/CartWishlistContext";
 const PaymentSuccess = () => {
   const navigate = useNavigate();
+  const { currentUser } = useSelector((state: any) => state.user);
+  const getCartAndWishlistCount = async () => {
+    try {
+      if (!currentUser) return;
+      const { setCartCount, setWishlistCount } =
+        useContext(CartWishlistContext);
+      const fetch1 = fetch(
+        import.meta.env.VITE_API_BASE_URL + "/api/get-cart-count",
+        {
+          headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      ).then((res) => res.json());
+      const fetch2 = fetch(
+        import.meta.env.VITE_API_BASE_URL + "/api/get-wishlist-count",
+        {
+          headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      ).then((res) => res.json());
+      const [res1, res2] = await Promise.all([fetch1, fetch2]);
+      if (!res1.success || !res2.success) {
+        throw new Error(res1.success ? res2.message : res1.message);
+      }
+      setCartCount(res1.count);
+      setWishlistCount(res2.count);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getCartAndWishlistCount();
   setTimeout(() => {
-    location.href = "/";
+    navigate("/");
   }, 3000);
   return (
     <div className="fixed h-full w-full flex flex-col  items-center pt-64 md:pt-32">
@@ -29,8 +64,7 @@ const PaymentSuccess = () => {
         transition={{ duration: 0.5, delay: 0.7 }}
         className="mt-4 text-base w-72 md:w-96 text-center"
       >
-        Thank you for your purchase. You will receive an email with your order
-        details shortly. Redirecting to home page...
+        Thank you for your purchase. Redirecting to home page...
       </motion.p>
     </div>
   );
