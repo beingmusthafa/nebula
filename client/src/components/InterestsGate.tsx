b2c6bb617116a180f0d1fab23726918211a96105
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-04-07 01:10:53 +0530

    BUGFIX: optional chaining

--- client/src/components/InterestsGate.tsx
+++ client/src/components/InterestsGate.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const InterestsGate = () => {
   const { currentUser } = useSelector((state: any) => state.user);
-  if (!currentUser || currentUser?.interests.length > 0) {
+  if (!currentUser || currentUser?.interests?.length > 0) {
     return <Outlet />;
   }
   return <Navigate to={"/add-interests"} />;
